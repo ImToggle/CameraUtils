7fed63c8e0c116df3b362a607a106ebc93971534
ImToggle <98242902+ImToggle@users.noreply.github.com>
2023-11-05 00:03:06 +0800

    100% not copied from patcher

--- src/main/java/me/imtoggle/camerautils/mixin/HotbarScrolling.java
+++ src/main/java/me/imtoggle/camerautils/mixin/HotbarScrolling.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InventoryPlayer.class)
 public class HotbarScrolling {
     @Inject(method = "changeCurrentItem", at = @At("HEAD"), cancellable = true)
-    private void patcher$cancelScrolling(int direction, CallbackInfo ci) {
+    private void cancelScrolling(int direction, CallbackInfo ci) {
         if (Config.keyBind.isActive()) {
             ci.cancel();
         }
